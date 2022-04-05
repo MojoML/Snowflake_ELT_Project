a884019f67d3ebc0e4ceaa6eb94873fe32f99719
Mory Kaba <60922141+MojoML@users.noreply.github.com>
2022-04-05 11:37:33 +0200

    Update create_fact_table.sql

--- loading/create_fact_table.sql
+++ loading/create_fact_table.sql
@@ -1,6 +1,15 @@
- -- SELECT * FROM foreign_trade_processed
+  CREATE OR REPLACE TABLE trade_fact (
+    country_id INT REFERENCES dim_country(country_id),
+    commodity_id INT REFERENCES dim_commodity(commodity_id),
+    date_id INT REFERENCES dim_date(date_id),
+    imports_net_mass_tons INT,
+    imports_value_eur INT, 
+    imports_value_usd INT, 
+    exports_net_mass_tons INT,
+    exports_value_eur INT,
+    exports_value_usd INT);
  
- CREATE OR REPLACE TABLE trade_fact AS 
+ INSERT INTO trade_fact  
      (SELECT 
         c.country_id, cm.commodity_id, d.date_id,
         imports_net_mass_tons, imports_value_eur, imports_value_usd,
